a0af19bfd7513bbe488ba8af73fdd6aca6cf21c0
Erick Leite <147015346+Erick-Leite@users.noreply.github.com>
2023-10-30 10:58:28 -0300

    Add files via upload

--- maxWidth.js
+++ maxWidth.js
@@ -3,33 +3,29 @@ const container = document.querySelector(".container");
 
 if(width < 576){
     container.style.maxWidth=`100%`;
-}else if(width >= 576 && width < 768){
+ }else if(width >= 576 && width < 768){
     container.style.maxWidth=`93.75vw`;
-}else if(width >= 768 && width < 980){
+ }else if(width >= 768 && width < 980){
     container.style.maxWidth=`72.0rem`;
-}else if(width >= 980 && width < 992){
+ }else if(width >= 980 && width < 1200){
     container.style.maxWidth=`73.469vw`;
-}else if(width >= 992 && width < 1200){
-    container.style.maxWidth=`75vw`;
-}else if(width >= 1200){
+ }else if(width >= 1200){
     container.style.maxWidth=`90.0rem`;
 }
 
 
 const maxWidth = () => {
-  if(width < 576){
+ if(width < 576){
     container.style.maxWidth=`100%`;
-}else if(width >= 576 && width < 768){
+ }else if(width >= 576 && width < 768){
     container.style.maxWidth=`93.75vw`;
-}else if(width >= 768 && width < 980){
+ }else if(width >= 768 && width < 980){
     container.style.maxWidth=`72.0rem`;
-}else if(width >= 980 && width < 992){
+ }else if(width >= 980 && width < 1200){
     container.style.maxWidth=`73.469vw`;
-}else if(width >= 992 && width < 1400){
-    container.style.maxWidth=`75vw`;
-}else if(width >= 1200){
+ }else if(width >= 1200){
     container.style.maxWidth=`90.0rem`;
-}
+ }
 
 }
 
