4e050c00d471e6e8f935d716bba5986a01cb5816
Erick Leite <147015346+Erick-Leite@users.noreply.github.com>
2023-11-16 14:01:37 -0300

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,4 +1,32 @@
-function checkAnswer(input_of_alternatives, alternative, alternatives_answers, result, questions, score, all_hits, restart, explanation_button){
+let popup_is_showing = false;
+
+function handle_popup(event){
+  if(popup_is_showing === true){
+      return;
+  }
+    
+  let popup = document.createElement("div");
+  popup.innerHTML = `VOCÊ PRECISA <a href="#restart">RECOMEÇAR</a> O QUESTIONÁRIO PARA VOLTAR A MARCAR.<span class="close">X</span>`;
+  popup.style.left = `${event.pageX}px`;
+  popup.style.top = `${event.pageY}px`;
+  popup.classList += "popup";
+  document.body.appendChild(popup);
+  popup_is_showing = true;
+  const close = document.querySelector(".close");
+  close.addEventListener('click', () => {
+    document.body.removeChild(popup);
+    popup_is_showing = false;
+  })
+  
+  setTimeout(() => {
+    document.body.removeChild(popup);
+    popup_is_showing = false;
+  }, 2500)
+    
+}
+
+
+function checkAnswer(input_of_alternatives, option_of_alternative, alternative, alternatives_answers, result, questions, score, all_hits, restart, explanation_button){
 
 input_of_alternatives.forEach((valorAtual, index) => {
   let alternative_input = input_of_alternatives[index];
@@ -18,6 +46,15 @@ alternative[index].classList.add("incorrect_answers");
     }
     
     input_of_alternatives[index].disabled = true;
+    
+    if (input_of_alternatives[index].disabled === true) {
+      option_of_alternative.forEach((option) => {
+     option.addEventListener("click", handle_popup); 
+      
+      })    
+        
+    }
+    
     
  })
      
@@ -40,12 +77,19 @@ alternative[index].classList.add("incorrect_answers");
 }
 
 
-function restartQuiz(input_of_alternatives, alternative, score, result, restart, all_hits, explanation_button, answers){
+function restartQuiz(input_of_alternatives, option_of_alternative, alternative, score, result, restart, all_hits, explanation_button, answers){
   
   input_of_alternatives.forEach((alt_in, index) => {
 
   input_of_alternatives[index].checked = false;  
   input_of_alternatives[index].disabled = false;
+  
+  if (input_of_alternatives[index].disabled === false) {
+    option_of_alternative.forEach((option) => {
+     option.removeEventListener("click", handle_popup);
+    });
+  }
+  
   
 input_of_alternatives[index].classList.remove("correct_answers", "incorrect_answers");
 
@@ -70,7 +114,7 @@ answers[index].classList.add("hidden");
   })
   
   answers.forEach((answer, index) => {
- answers[index].classList.remove("show");
+   answers[index].classList.remove("show");
 });
   
 }
--- newton_questions.js
+++ newton_questions.js
@@ -1,6 +1,6 @@
 import {checkAnswer, restartQuiz, show_hide} from './functions.js';
 
-const alternatives_answers = ["c1", "a2", "b3", "d4", "c5", "c6", "b7", "e8", "a9", "b10"]
+const alternatives_answers = ["c1", "a2", "b3", "d4", "e5", "c6", "b7", "e8", "a9", "d10"]
 
 let score = 0;
 
@@ -11,15 +11,15 @@ const restart = document.getElementById("restart");
 const explanation_button = [...document.querySelectorAll(".explanation_button")];
 const input_of_alternatives = document.querySelectorAll("input[type=radio]");
 const alternative = document.querySelectorAll(".alternative");
+const option_of_alternative = document.querySelectorAll(".option label");
 const answers = [...document.getElementsByClassName("answers")];
-
 
 explanation_button.forEach((button, index) => {
   button.addEventListener("click", () => show_hide(index, answers, explanation_button))
 });
 
 
-restart.addEventListener("click", () => {restartQuiz(input_of_alternatives, alternative, score, result, restart, all_hits, explanation_button, answers)});
+restart.addEventListener("click", () => {restartQuiz(input_of_alternatives, option_of_alternative, alternative, score, result, restart, all_hits, explanation_button, answers)});
 
 
-all_hits.addEventListener("click", () => {checkAnswer(input_of_alternatives, alternative, alternatives_answers, result, questions, score, all_hits, restart, explanation_button)});
+all_hits.addEventListener("click", () => {checkAnswer(input_of_alternatives, option_of_alternative, alternative, alternatives_answers, result, questions, score, all_hits, restart, explanation_button)});
